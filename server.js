a869d7fd014ffd9bb4324722c25e6fb6e8176e7f
Pedro <71204668+sprayx@users.noreply.github.com>
2023-07-14 00:54:19 -0300

    badges

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const config = require("./config.json")
+const { Client, GatewayIntentBits } = require('discord.js');
+const axios = require('axios');
+const config = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
 
 client.on('ready', () => {
   console.log(`Bot está online com o usuário ${client.user.tag}`);
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-//informacoes do usuario por get.
+// Informações do usuário por GET
 app.get('/userinfo/:userId', async (req, res) => {
   const userId = req.params.userId;
   const user = await client.users.fetch(userId);
@@ -37,17 +37,41 @@ app.get('/userinfo/:userId', async (req, res) => {
     const hours = date.getHours().toString().padStart(2, '0');
     const minutes = date.getMinutes().toString().padStart(2, '0');
     const seconds = date.getSeconds().toString().padStart(2, '0');
-  
+
     return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
   }
+
+  async function getBadges(userId) {
+    try {
+      const response = await axios.get(`https://discord.com/api/v9/users/${userId}`, {
+        headers: {
+          Authorization: `Bot ${config.token}`,
+          'Content-Type': 'application/json',
+        },
+      });
+      const user = response.data;
+  
+      if (user.public_flags && typeof user.public_flags.toArray === 'function') {
+        const badges = user.public_flags.toArray().map((flag) => `https://cdn.discordapp.com/attachments/876658705442764032/876660407743193364/${flag}.png`);
+        return badges;
+      }
+      return [];
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  }
+
+  const badges = await getBadges(userId);
+
   const userInfo = {
     avatarURL,
     username,
     id,
     banner,
     formattedDate,
+    badges,
   };
-  console.log(user)
   res.json(userInfo);
 });
 
